f0c0c7140f73e14aea2cd574e390745cbb9cfcde
Martin Ohl <Martin.Ohl@ohl-net.eu>
2019-11-26 13:32:56 +0100

    v.0.2
    Changelog: added getpass to avoid pw visibility in bash history (thx to secufred)

---
+++ activity-feeds/mvision_edr_activity_feeds.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# Written by mohlcyber v.0.2 (26.11.2019).
+# Changelog: added getpass to avoid pw visibility in bash history (thx to secufred)
+
+import getpass
+import json
+import socket
+
+from datetime import datetime
+from dxlstreamingclient.channel import Channel, ChannelAuth
+
+# Credentials MVISION EDR
+URL = 'https://api.soc.mcafee.com/'
+USER = 'user@email.com'
+
+# IP Address of the Syslog server - Receiver
+SYSLOG_SERVER = '1.1.1.1'
+SYSLOG_PORT = 514
+
+# Topics to subscribe
+TOPICS = ['case-mgmt-events', 'BusinessEvents', 'threatEvents']
+
+class EDR():
+    def __init__(self):
+        self.url = URL
+        self.user = USER
+        self.pw = PW
+        self.topics = TOPICS
+
+        self.auth = ChannelAuth(self.url, self.user, self.pw, verify_cert_bundle='')
+
+    def activity_feed(self):
+        try:
+            with Channel(URL, auth=self.auth, consumer_group='mvisionedr_events', verify_cert_bundle='') as channel:
+
+                def process_callback(payloads):
+
+                    if not payloads == []:
+                        for payload in payloads:
+                            print(json.dumps(payload))
+                            Log().syslogger(json.dumps(payload, separators=(",", ":"), sort_keys=True))
+
+                    return True
+
+                channel.run(process_callback, wait_between_queries=5, topics=self.topics)
+
+        except Exception as e:
+            print("Unexpected error: {}".format(e))
+
+
+class Log():
+    def logger(self, msg):
+        t = datetime.now()
+        print(msg)
+        with open('log.txt', 'a') as logfile:
+            logfile.write('{}: {}\n'.format(t, msg))
+            logfile.close()
+
+    def syslogger(self, event):
+        time = datetime.today().strftime('%b %d %H:%M:%S')
+        msg = time + ' MVISION EDR[0]: ' + event
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto(msg.encode(), (SYSLOG_SERVER, SYSLOG_PORT))
+        sock.close()
+
+
+if __name__ == "__main__":
+    try:
+        PW = getpass.getpass()
+    except Exception as error:
+        print('Error', error)
+    else:
+        edr = EDR()
+        edr.activity_feed()
