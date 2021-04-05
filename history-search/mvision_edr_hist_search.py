10a9c8de05499f7c1ce1f4d173e11d80a354cee5
Martin Ohl <Martin.Ohl@ohl-net.eu>
2021-04-05 22:21:04 +0200

    v.0.6
    added search variable, get all historical details, changed to utc time search.

--- history-search/mvision_edr_hist_search.py
+++ history-search/mvision_edr_hist_search.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Written by mohlcyber v.0.5 (01.04.2021)
+# Written by mohlcyber v.0.6 (05.04.2021)
 # Script to query historical data
 
 import sys
@@ -8,6 +8,7 @@ import argparse
 import requests
 import json
 import time
+import logging
 
 from datetime import datetime, timedelta
 
@@ -22,6 +23,14 @@ class EDR():
             self.base_url = 'https://api.soc.ap-southeast-2.mcafee.com'
 
         self.verify = True
+
+        self.logger = logging.getLogger('logs')
+        self.logger.setLevel('DEBUG')
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s")
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+
         self.request = requests.Session()
 
         user = args.user
@@ -33,6 +42,7 @@ class EDR():
         self.days = args.days
         self.limit = args.limit
         self.type = args.type
+        self.search = args.search
 
         self.pattern = '%Y-%m-%dT%H:%M:%S.%fZ'
 
@@ -43,9 +53,9 @@ class EDR():
         if r.status_code == 200:
             token = res['AuthorizationToken']
             self.headers = {'Authorization': 'Bearer {}'.format(token)}
-            print('AUTHENTICATION: Successfully authenticated.')
+            self.logger.debug('AUTHENTICATION: Successfully authenticated.')
         else:
-            print('ERROR: Something went wrong during the authentication')
+            self.logger.error('Something went wrong during the authentication')
             sys.exit()
 
     def get_host(self):
@@ -60,20 +70,20 @@ class EDR():
                     maGuid = host['maGuid']
                     return maGuid
             elif res.json()['total'] > 1:
-                print('ERROR: To many hosts found with this Hostname. Please be more specfic.')
-                print(res.json())
+                self.logger.error('Too many hosts found with this Hostname. Please be more specfic.')
+                self.logger.error(res.json())
                 sys.exit()
             else:
-                print('ERROR: Could not find a Host with this Hostname.')
+                self.logger.error('Could not find a Host with this Hostname.')
                 sys.exit()
 
-        except Exception as e:
-            print('ERROR: Something went wrong in edr.get_hosts. Error: {}'.format(str(e)))
+        except Exception as error:
+            self.logger.error('Error in edr.get_hosts. Error: {}'.format(str(error)))
 
     def hist_search(self):
         try:
-            t_now = datetime.now().strftime(self.pattern)
-            t_before = (datetime.now() - timedelta(days=self.days)).strftime(self.pattern)
+            t_now = datetime.utcnow().strftime(self.pattern)
+            t_before = (datetime.utcnow() - timedelta(days=self.days)).strftime(self.pattern)
 
             query = {
               "$filter": {
@@ -89,30 +99,36 @@ class EDR():
                     }
                   }
                 ],
-                "maGuid": self.get_host(),
-                "tags": "@AC."+self.type
+                "maGuid": self.get_host()
               },
               "$sort": {
                 "time": -1
               }
             }
 
+            if self.type is not None:
+                query['$filter']['tags'] = '@AC.' + self.type
+
+            if self.search is not None:
+                query['$filter']['$and'][0]['$term'] = self.search
+
             res = self.request.get(self.base_url + '/ltc/api/v1/ltc/query/traces/?query={}&limit={}&skip=0'
                                    .format(json.dumps(query), self.limit),
-                                    headers=self.headers)
+                                   headers=self.headers)
 
             if res.status_code != 200:
-                print('ERROR: edr.detect_search - {0} - {1}'.format(str(res.status_code), res.text))
+                self.logger.error('edr.detect_search - {0} - {1}'.format(str(res.status_code), res.text))
             else:
-                print(res.json())
+                self.logger.info(res.json())
+                self.logger.info('Found {0} items.'.format(res.json()['count']))
 
         except Exception as error:
-            print('ERROR: Something went wrong in edr.hist_search. Error: {}'.format(str(error)))
+            self.logger.error('Error in edr.hist_search. Error: {}'.format(str(error)))
 
     def detect_search(self):
         try:
-            t_now = datetime.now().strftime(self.pattern)
-            t_before = (datetime.now() - timedelta(days=self.days)).strftime(self.pattern)
+            t_now = datetime.utcnow().strftime(self.pattern)
+            t_before = (datetime.utcnow() - timedelta(days=self.days)).strftime(self.pattern)
 
             epoch_now = int(time.mktime(time.strptime(t_now, self.pattern)))
             epoch_before = int(time.mktime(time.strptime(t_before, self.pattern)))
@@ -132,16 +148,18 @@ class EDR():
                                    headers=self.headers)
 
             if res.status_code != 200:
-                print('ERROR: edr.detect_search - {0} - {1}'.format(str(res.status_code), res.text))
+                self.logger.error('Error in edr.detect_search - {0} - {1}'.format(str(res.status_code), res.text))
             else:
-                print(res.json())
+                self.logger.info(res.json())
+                self.logger.info('Found {0} items.'.format(res.json()['count']))
 
         except Exception as error:
-            print('ERROR: Something went wrong in edr.detect_search. Error: {}'.format(str(error)))
+            self.logger.error('Error in edr.detect_search. Error: {}'.format(str(error)))
 
 
 if __name__ == '__main__':
-    usage = """python mvision_edr_hist_search.py -R <REGION> -U <USERNAME> -P <PASSWORD> -H <HOSTNAME> -T <SEARCHTYPE> -D <DAYS> -L <MAX RESULTS>"""
+    usage = """python mvision_edr_hist_search.py -R <REGION> -U <USERNAME> -P <PASSWORD> -H <HOSTNAME> """\
+            """-T <TYPE> -S <SEARCH> -D <DAYS> -L <MAX RESULTS>"""
     title = 'McAfee EDR Python API'
     parser = argparse.ArgumentParser(description=title)
 
@@ -162,7 +180,7 @@ if __name__ == '__main__':
                         help='Hostname to Query')
 
     parser.add_argument('--type', '-T',
-                        required=True, type=str,
+                        required=False, type=str,
                         help='Search Type', choices=[
                             'APICall',
                             'ProcessCreated',
@@ -185,6 +203,10 @@ if __name__ == '__main__':
                             'DetectionsAlerts',
                             'Alerts']
                         )
+
+    parser.add_argument('--search', '-S',
+                        required=False, type=str,
+                        help='Search Term')
 
     parser.add_argument('--days', '-D',
                         required=True, type=int,
