67080ebcd2e946fcb0b79636224ad294e53fff03
Martin Ohl <Martin.Ohl@ohl-net.eu>
2022-04-25 16:23:41 +0200

    v.1.0
    Updated auth from username/password to client_id/client_secret

--- real-time-search-reaction/mvision_edr_search_process.py
+++ real-time-search-reaction/mvision_edr_search_process.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-# Written by mohlcyber v.0.1 (17.02.2021)
-# based on a process name script will automatically launch MVISION EDR query
+# Written by mohlcyber v.1.0 (25.04.2022)
+# based on a hash, script will automatically launch MVISION EDR query
 
 import sys
 import getpass
@@ -14,212 +14,291 @@ from argparse import ArgumentParser, RawTextHelpFormatter
 
 class EDR():
     def __init__(self):
-        self.logger = logging.getLogger('logs')
-        loglevel = args.loglevel
-        self.logger.setLevel(loglevel)
-        ch = logging.StreamHandler()
-        formatter = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s")
-        ch.setFormatter(formatter)
-        self.logger.addHandler(ch)
-
+        self.iam_url = 'iam.mcafee-cloud.com/iam/v1.1'
         if args.region == 'EU':
-            self.base_url = 'https://api.soc.eu-central-1.mcafee.com'
-        elif args.region == 'US':
-            self.base_url = 'https://api.soc.mcafee.com'
+            self.base_url = 'soc.eu-central-1.mcafee.com'
+        elif args.region == 'US-W':
+            self.base_url = 'soc.mcafee.com'
+        elif args.region == 'US-E':
+            self.base_url = 'soc.us-east-1.mcafee.com'
         elif args.region == 'SY':
-            self.base_url = 'https://api.soc.ap-southeast-2.mcafee.com'
+            self.base_url = 'soc.ap-southeast-2.mcafee.com'
+        elif args.region == 'GOV':
+            self.base_url = 'soc.mcafee-gov.com'
 
-        self.verify = True
-        self.request = requests.Session()
+        self.logging()
 
-        user = args.user
-        pw = args.password
-        creds = (user, pw)
+        self.session = requests.Session()
+        self.session.verify = True
+
+        creds = (args.client_id, args.client_secret)
         self.auth(creds)
 
         self.pname = args.process
 
-    def auth(self, creds):
-        r = requests.get(self.base_url + '/identity/v1/login', auth=creds)
-        res = r.json()
+    def logging(self):
+        self.logger = logging.getLogger('logs')
+        self.logger.setLevel(args.loglevel.upper())
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s")
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
 
-        if r.status_code == 200:
-            token = res['AuthorizationToken']
-            headers = {'Authorization': 'Bearer {}'.format(token)}
-            self.request.headers.update(headers)
-            self.logger.info('Successfully authenticated.')
-        else:
-            self.logger.error('Something went wrong during the authentication')
-            sys.exit()
+    def auth(self, creds):
+        try:
+            payload = {
+                'scope': 'mi.user.investigate soc.act.tg soc.hts.c soc.hts.r soc.rts.c soc.rts.r soc.qry.pr',
+                'grant_type': 'client_credentials',
+                'audience': 'mcafee'
+            }
+
+            headers = {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+
+            res = self.session.post('https://{0}/token'.format(self.iam_url), headers=headers, data=payload, auth=creds)
+
+            self.logger.debug('request url: {}'.format(res.url))
+            self.logger.debug('request headers: {}'.format(res.request.headers))
+            self.logger.debug('request body: {}'.format(res.request.body))
+
+            if res.ok:
+                token = res.json()['access_token']
+                self.session.headers = {'Authorization': 'Bearer {}'.format(token)}
+                self.logger.debug('AUTHENTICATION: Successfully authenticated.')
+            else:
+                self.logger.error('Error in edr.auth(). Error: {0} - {1}'
+                                  .format(str(res.status_code), res.text))
+                exit()
+
+        except Exception as error:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            self.logger.error("Error in {location}.{funct_name}() - line {line_no} : {error}"
+                              .format(location=__name__, funct_name=sys._getframe().f_code.co_name,
+                                      line_no=exc_tb.tb_lineno, error=str(error)))
 
     def search(self):
-        queryId = None
-
-        payload = {
-            "projections": [
-                {
-                    "name": "HostInfo",
-                    "outputs": ["hostname", "ip_address"]
-                }, {
-                    "name": "Processes",
-                    "outputs": ["name", "id", "parentimagepath", "started_at"]
-                }
-            ],
-            "condition": {
-                "or": [{
-                    "and": [{
-                        "name": "Processes",
-                        "output": "name",
-                        "op": "CONTAINS",
-                        "value": str(self.pname)
-                    }]
-                }]
-            }
-        }
-
-        res = self.request.post(self.base_url + '/active-response/api/v1/searches', json=payload)
         try:
-            if res.status_code == 200:
+            queryId = None
+
+            payload = {
+                "projections": [
+                    {
+                        "name": "HostInfo",
+                        "outputs": ["hostname", "ip_address"]
+                    }, {
+                        "name": "Processes",
+                        "outputs": ["name", "id", "parentimagepath", "started_at"]
+                    }
+                ],
+                "condition": {
+                    "or": [{
+                        "and": [{
+                            "name": "Processes",
+                            "output": "name",
+                            "op": "CONTAINS",
+                            "value": str(self.pname)
+                        }]
+                    }]
+                }
+            }
+
+            res = self.session.post('https://api.{0}/active-response/api/v1/searches'.format(self.base_url), json=payload)
+
+            self.logger.debug('request url: {}'.format(res.url))
+            self.logger.debug('request headers: {}'.format(res.request.headers))
+            self.logger.debug('request body: {}'.format(res.request.body))
+
+            if res.ok:
                 queryId = res.json()['id']
                 self.logger.info('MVISION EDR search got started successfully')
             else:
-                self.logger.error('Could not find the query ID.')
-        except Exception as e:
-            self.logger.error('Could not find the query ID. Error: {}'.format(e))
-            sys.exit()
+                self.logger.error('Error in edr.search(). Error {} - {}'.format(str(res.status_code), res.text))
+                exit()
 
-        return queryId
+            return queryId
+
+        except Exception as error:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            self.logger.error("Error in {location}.{funct_name}() - line {line_no} : {error}"
+                              .format(location=__name__, funct_name=sys._getframe().f_code.co_name,
+                                      line_no=exc_tb.tb_lineno, error=str(error)))
 
     def search_status(self, queryId):
-        status = False
-        res = self.request.get(self.base_url + '/active-response/api/v1/searches/{}/status'.format(str(queryId)))
-        if res.status_code == 200:
-            if res.json()['status'] == 'FINISHED':
-                status = True
-            else:
-                self.logger.info('Search still in process. Status: {}'.format(res.json()['status']))
-        return status
+        try:
+            status = False
+            res = self.session.get('https://api.{0}/active-response/api/v1/searches/{1}/status'.format(self.base_url, str(queryId)))
+
+            self.logger.debug('request url: {}'.format(res.url))
+            self.logger.debug('request headers: {}'.format(res.request.headers))
+            self.logger.debug('request body: {}'.format(res.request.body))
+
+            if res.ok:
+                if res.json()['status'] == 'FINISHED':
+                    status = True
+                else:
+                    self.logger.info('Search still in process. Status: {}'.format(res.json()['status']))
+            return status
+        except Exception as error:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            self.logger.error("Error in {location}.{funct_name}() - line {line_no} : {error}"
+                              .format(location=__name__, funct_name=sys._getframe().f_code.co_name,
+                                      line_no=exc_tb.tb_lineno, error=str(error)))
 
     def search_result(self, queryId):
-        res = self.request.get(self.base_url + '/active-response/api/v1/searches/{}/results'.format(str(queryId)))
-        if res.status_code == 200:
-            try:
-                items = res.json()['totalItems']
+        try:
+            res = self.session.get('https://api.{0}/active-response/api/v1/searches/{1}/results'.format(self.base_url, str(queryId)))
 
-                react_summary = []
-                for item in res.json()['items']:
-                    react_dict = {}
-                    react_dict[item['id']] = item['output']['Processes|id']
-                    react_summary.append(react_dict)
+            self.logger.debug('request url: {}'.format(res.url))
+            self.logger.debug('request headers: {}'.format(res.request.headers))
+            self.logger.debug('request body: {}'.format(res.request.body))
 
-                self.logger.debug(json.dumps(res.json()))
-                self.logger.info('MVISION EDR search got {} responses for this process name. {} '
-                                 .format(items, len(react_summary)))
+            if res.ok:
+                try:
+                    items = res.json()['totalItems']
+                    react_summary = []
+                    for item in res.json()['items']:
+                        react_dict = {}
+                        react_dict[item['id']] = item['output']['Processes|id']
+                        react_summary.append(react_dict)
 
-                return react_summary
+                    self.logger.debug(json.dumps(res.json()))
+                    self.logger.info('MVISION EDR search got {} responses for this process name. {}'
+                                     .format(items, len(react_summary)))
 
-            except Exception as e:
-                self.logger.error('Something went wrong to retrieve the results. Error: {}'.format(e))
-                sys.exit()
-        else:
-            self.logger.error('Something went wrong to retrieve the results.')
-            sys.exit()
+                    return react_summary
+
+                except Exception as e:
+                    self.logger.error('Something went wrong to retrieve the results. Error: {}'.format(e))
+                    exit()
+            else:
+                self.logger.error('Error in edr.search_result(). Error {} - {}'.format(str(res.status_code), res.text))
+                exit()
+
+        except Exception as error:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            self.logger.error("Error in {location}.{funct_name}() - line {line_no} : {error}"
+                              .format(location=__name__, funct_name=sys._getframe().f_code.co_name,
+                                      line_no=exc_tb.tb_lineno, error=str(error)))
 
     def get_reactions(self):
-        res = self.request.get(self.base_url + '/active-response/api/v1/catalog/reactions')
+        try:
+            res = self.session.get('https://api.{0}/active-response/api/v1/catalog/reactions'.format(self.base_url))
 
-        if res.status_code == 200:
-            return res.json()
-        else:
-            self.logger.error('Something went wrong to retrieve reactions.: {0}'.format(str(res.text)))
+            if res.ok:
+                return res.json()
+            else:
+                self.logger.error('Error in edr.get_reactions(). Error {} - {}'.format(str(res.status_code), res.text))
+                exit()
+
+        except Exception as error:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            self.logger.error("Error in {location}.{funct_name}() - line {line_no} : {error}"
+                              .format(location=__name__, funct_name=sys._getframe().f_code.co_name,
+                                      line_no=exc_tb.tb_lineno, error=str(error)))
 
     def reaction_execution(self, queryId, systemId, pid):
-        reactionId = None
+        try:
+            payload = {
+                "action": "killProcess",
+                "searchResultsArguments": {
+                    "searchId": int(queryId),
+                    "rowsIds": [str(systemId)],
+                    "arguments": {}
+                },
+                "provider": "AR",
+                "actionInputs": [
+                    {
+                        "name": "pid",
+                        "value": str(pid)
+                    }
+                ]
+            }
 
-        payload = {
-            "action":"killProcess",
-            "searchResultsArguments": {
-                "searchId": int(queryId),
-                "rowsIds": [str(systemId)],
-                "arguments": {}
-            },
-            "provider": "AR",
-            "actionInputs": [
-                {
-                    "name": "pid",
-                    "value": str(pid)
-                }
-            ]
-        }
+            res = self.session.post('https://api.{0}/remediation/api/v1/actions/search-results-actions'.format(self.base_url),
+                                    json=payload)
 
-        res = self.request.post(self.base_url + '/remediation/api/v1/actions/search-results-actions',
-                                json=payload)
+            self.logger.debug('request url: {}'.format(res.url))
+            self.logger.debug('request headers: {}'.format(res.request.headers))
+            self.logger.debug('request body: {}'.format(res.request.body))
 
-        if res.status_code == 201:
-            try:
-                reactionId = res.json()['id']
+            if res.ok:
+                rid = res.json()['id']
                 self.logger.info('MVISION EDR reaction got executed successfully')
-            except Exception as e:
-                self.logger.error('Something went wrong to create reaction. Error: {}'.format(e))
-                sys.exit()
+                return rid
+            else:
+                self.logger.error('Error in edr.reaction_execution(). Error {} - {}'.format(str(res.status_code), res.text))
+                exit()
 
-        return reactionId
+        except Exception as error:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            self.logger.error("Error in {location}.{funct_name}() - line {line_no} : {error}"
+                              .format(location=__name__, funct_name=sys._getframe().f_code.co_name,
+                                      line_no=exc_tb.tb_lineno, error=str(error)))
 
     def main(self):
-        #Retrieve all reactions
-        # reactions = self.get_reactions()
-        # self.logger.info(json.dumps(reactions))
-        # sys.exit()
+        try:
+            # Retrieve all reactions
+            # reactions = self.get_reactions()
+            # self.logger.info(json.dumps(reactions))
+            # sys.exit()
 
-        queryId = self.search()
-        if queryId is None:
-            sys.exit()
+            queryId = self.search()
+            if queryId is None:
+                exit()
 
-        while self.search_status(queryId) is False:
-            time.sleep(30)
+            while self.search_status(queryId) is False:
+                time.sleep(30)
 
-        results = self.search_result(queryId)
-        if results == []:
-            self.logger.info('No Further Actions need to take place.')
-            sys.exit()
+            results = self.search_result(queryId)
+            if len(results) == 0:
+                exit()
 
-        if args.reaction == 'True':
-            for result in results:
-                for systemId, pid in result.items():
-                    reaction_id = self.reaction_execution(queryId, systemId, pid)
+            if args.reaction == 'True':
+                for result in results:
+                    for systemId, filePath in result.items():
+                        reaction_id = self.reaction_execution(queryId, systemId, filePath)
 
-                    if reaction_id is None:
-                        self.logger.error('Could not create new MVISION EDR reaction')
+                        if reaction_id is None:
+                            self.logger.error('Could not create new MVISION EDR reaction')
+
+        except Exception as error:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            self.logger.error("Error in {location}.{funct_name}() - line {line_no} : {error}"
+                              .format(location=__name__, funct_name=sys._getframe().f_code.co_name,
+                                      line_no=exc_tb.tb_lineno, error=str(error)))
 
 
 if __name__ == '__main__':
-    usage = """Usage: mvision_edr_search_process.py -U <username> -P <password> -PN <process name> """
-    title = 'McAfee EDR Python API'
+    usage = """Usage: python mvision_edr_search_process.py -C <CLIENT_ID> -S <CLIENT_SECRET> -PN <process name>"""
+    title = 'MVISION EDR Python API'
     parser = ArgumentParser(description=title, usage=usage, formatter_class=RawTextHelpFormatter)
 
-    parser.add_argument('--region', '-R', required=True,
-                        choices=['EU', 'US', 'SY'], type=str,
-                        help='MVISION EDR Tenant Region')
+    parser.add_argument('--region', '-R',
+                        required=True, type=str,
+                        help='MVISION EDR Tenant Location', choices=['EU', 'US-W', 'US-E', 'SY', 'GOV'])
 
-    parser.add_argument('--user', '-U', required=True,
-                        type=str, help='MVISION EDR User Account')
+    parser.add_argument('--client_id', '-C',
+                        required=True, type=str,
+                        help='MVISION EDR Client ID')
 
-    parser.add_argument('--password', '-P', required=False,
-                        type=str, help='MVISION EDR Password')
+    parser.add_argument('--client_secret', '-S',
+                        required=False, type=str,
+                        help='MVISION EDR Client Secret')
 
     parser.add_argument('--process', '-PN', required=True,
                         type=str, default='Process Name to search for')
 
     parser.add_argument('--reaction', '-RE', required=False,
                         type=str, choices=['True', 'False'],
-                        default='False', help='Kill Process.')
+                        default='False', help='Kill Process')
 
     parser.add_argument('--loglevel', '-L', required=False,
                         type=str, choices=['INFO', 'DEBUG'],
-                        default='INFO', help='Specify log level.')
+                        default='INFO', help='Specify log level')
 
     args = parser.parse_args()
-    if not args.password:
-        args.password = getpass.getpass()
+    if not args.client_secret:
+        args.client_secret = getpass.getpass(prompt='MVISION EDR Client Secret: ')
 
     EDR().main()
