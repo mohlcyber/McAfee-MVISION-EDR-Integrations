2b1ec4ad4489251c77e27817745b5c32d0f9ee3a
Martin Ohl <Martin.Ohl@ohl-net.eu>
2022-04-25 13:06:01 +0200

    v.1.0

--- threats-monitoring/mvision_edr_threats_reaction.py
+++ threats-monitoring/mvision_edr_threats_reaction.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-# Written by mohlcyber v.0.1 (18.11.2021)
-# Script to retrieve threats from the monitoring dashboard and stop the related process
+# Written by mohlcyber v.1.0 (25.04.2022)
+# Script to retrieve all threats from the monitoring dashboard and stop the related process
 
 import sys
 import getpass
@@ -12,10 +12,12 @@ import os
 
 from argparse import ArgumentParser, RawTextHelpFormatter
 from datetime import datetime, timedelta
+from logging.handlers import SysLogHandler
 
 
 class EDR():
     def __init__(self):
+        self.iam_url = 'iam.mcafee-cloud.com/iam/v1.1'
         if args.region == 'EU':
             self.base_url = 'soc.eu-central-1.mcafee.com'
         elif args.region == 'US-W':
@@ -27,14 +29,12 @@ class EDR():
         elif args.region == 'GOV':
             self.base_url = 'soc.mcafee-gov.com'
 
-        self.verify = True
         self.logging()
 
-        self.request = requests.Session()
+        self.session = requests.Session()
+        self.session.verify = True
 
-        user = args.user
-        pw = args.password
-        creds = (user, pw)
+        creds = (args.client_id, args.client_secret)
 
         self.pattern = '%Y-%m-%dT%H:%M:%S.%fZ'
         self.cache_fname = 'cache.log'
@@ -53,17 +53,17 @@ class EDR():
 
             self.last_check = (last_detection + timedelta(seconds=1)).strftime(self.pattern)
         else:
-            self.logger.debug('Cache does not exists. Pulling data from last 7 days.')
-            self.last_pulled = (datetime.now() - timedelta(days=7)).strftime(self.pattern)
-            self.last_check = (datetime.now() - timedelta(days=7)).strftime(self.pattern)
+            self.logger.debug('Cache does not exists. Pulling data from last 14 days.')
+            self.last_pulled = (datetime.now() - timedelta(days=14)).strftime(self.pattern)
+            self.last_check = (datetime.now() - timedelta(days=14)).strftime(self.pattern)
 
-        self.limit = args.limit
-
+        self.limit = '2000'
         self.auth(creds)
 
     def logging(self):
+        # setup the console logger
         self.logger = logging.getLogger('logs')
-        self.logger.setLevel('DEBUG')
+        self.logger.setLevel(args.loglevel.upper())
         handler = logging.StreamHandler()
         formatter = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s")
         handler.setFormatter(formatter)
@@ -71,19 +71,30 @@ class EDR():
 
     def auth(self, creds):
         try:
-            res = self.request.get('https://api.' + self.base_url + '/identity/v1/login', auth=creds)
+            payload = {
+                'scope': 'soc.hts.c soc.hts.r soc.rts.c soc.rts.r soc.qry.pr',
+                'grant_type': 'client_credentials',
+                'audience': 'mcafee'
+            }
+
+            headers = {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+
+            res = self.session.post('https://{0}/token'.format(self.iam_url), headers=headers, data=payload, auth=creds)
+
+            self.logger.debug('request url: {}'.format(res.url))
+            self.logger.debug('request headers: {}'.format(res.request.headers))
+            self.logger.debug('request body: {}'.format(res.request.body))
 
             if res.ok:
-                token = res.json()['AuthorizationToken']
-                self.request.headers = {
-                    'Authorization': 'Bearer {}'.format(token),
-                    'Content-Type': 'application/json'
-                }
-                self.logger.debug('Successfully authenticated')
+                token = res.json()['access_token']
+                self.session.headers = {'Authorization': 'Bearer {}'.format(token)}
+                self.logger.debug('AUTHENTICATION: Successfully authenticated.')
             else:
                 self.logger.error('Error in edr.auth(). Error: {0} - {1}'
                                   .format(str(res.status_code), res.text))
-                sys.exit()
+                exit()
 
         except Exception as error:
             exc_type, exc_obj, exc_tb = sys.exc_info()
@@ -94,18 +105,21 @@ class EDR():
     def get_threats(self):
         try:
             epoch_before = int(time.mktime(time.strptime(self.last_pulled, self.pattern)))
-            self.timer = str(epoch_before * 1000)
 
             filter = {}
             severities = ["s0", "s1", "s2", "s3", "s4", "s5"]
             filter['severities'] = severities
 
-            res = self.request.get(
+            res = self.session.get(
                 'https://api.{0}/ft/api/v2/ft/threats?sort=-lastDetected&filter={1}&from={2}&limit={3}'
-                .format(self.base_url, json.dumps(filter), self.timer, str(self.limit)))
+                .format(self.base_url, json.dumps(filter), str(epoch_before * 1000), str(self.limit)))
+
+            self.logger.debug('request url: {}'.format(res.url))
+            self.logger.debug('request headers: {}'.format(res.request.headers))
+            self.logger.debug('request body: {}'.format(res.request.body))
 
             if res.ok:
-                self.logger.info('SUCCESS: Successful retrieved threats.')
+                self.logger.debug('SUCCESS: Successful retrieved threats.')
 
                 res = res.json()
                 if len(res['threats']) > 0:
@@ -114,7 +128,6 @@ class EDR():
                     cache.close()
 
                     for threat in res['threats']:
-                        # Enrich with detections
                         detections = self.get_detections(threat['id'])
                         threat['url'] = 'https://ui.' + self.base_url + '/monitoring/#/workspace/72,TOTAL_THREATS,{0}'\
                             .format(threat['id'])
@@ -133,11 +146,11 @@ class EDR():
 
                 else:
                     self.logger.info('No new threats identified. Exiting. {0}'.format(res))
-                    sys.exit()
+                    exit()
             else:
                 self.logger.error('Error in edr.get_threats(). Error: {0} - {1}'
                                   .format(str(res.status_code), res.text))
-                sys.exit()
+                exit()
 
         except Exception as error:
             exc_type, exc_obj, exc_tb = sys.exc_info()
@@ -149,18 +162,16 @@ class EDR():
         try:
             last_detected = datetime.strptime(self.last_check, self.pattern)
 
-            params = {
-                'limit': '30',
-                'sort': '-rank',
-                'skip': '0'
-            }
+            res = self.session.get('https://api.' + self.base_url + '/ft/api/v2/ft/threats/{0}/detections'
+                                   .format(threatId))
 
-            res = self.request.get('https://api.{0}/ft/api/v2/ft/threats/{1}/affectedhosts'
-                                   .format(self.base_url, threatId), params=params)
+            self.logger.debug('request url: {}'.format(res.url))
+            self.logger.debug('request headers: {}'.format(res.request.headers))
+            self.logger.debug('request body: {}'.format(res.request.body))
 
             if res.ok:
                 detections = []
-                for detection in res.json()['affectedHosts']:
+                for detection in res.json()['detections']:
                     first_detected = datetime.strptime(detection['firstDetected'], '%Y-%m-%dT%H:%M:%SZ')
 
                     if first_detected >= last_detected:
@@ -170,7 +181,7 @@ class EDR():
             else:
                 self.logger.error('Error in retrieving edr.get_detections(). Error: {0} - {1}'
                                   .format(str(res.status_code), res.text))
-                sys.exit()
+                exit()
 
         except Exception as error:
             exc_type, exc_obj, exc_tb = sys.exc_info()
@@ -185,7 +196,7 @@ class EDR():
                 'threatId': int(tId)
             }
 
-            res = self.request.post('https://api.{0}/case-mgmt/v1/cases/threats'.format(self.base_url), json=data)
+            res = self.session.post('https://api.{0}/case-mgmt/v1/cases/threats'.format(self.base_url), json=data)
 
             if res.ok:
                 caseId = str(res.json()['_links']['self']['href']).split('/')
@@ -204,7 +215,7 @@ class EDR():
     def exec_reaction(self, caseId, tId, hId):
         try:
             data = {
-                'action': 'StopProcess', # alternative - StopAndRemove and Quarantine
+                'action': 'StopProcess', # options [StopProcess, StopAndRemove, QuarantineHost, UnquarantineHost]
                 'caseId': str(caseId),
                 'threatActionArguments': {
                     'threatId': str(tId),
@@ -214,7 +225,7 @@ class EDR():
                 }
             }
 
-            res = self.request.post('https://api.{0}/remediation/api/v1/actions/threat-actions'.format(self.base_url),
+            res = self.session.post('https://api.{0}/remediation/api/v1/actions/threat-actions'.format(self.base_url),
                                     data=json.dumps(data))
 
             if res.ok:
@@ -233,29 +244,29 @@ class EDR():
 
 
 if __name__ == '__main__':
-    usage = """python mvision_edr_threats.py -R <REGION> -U <USERNAME> -P <PASSWORD> -D <DETAILS> -L <MAX RESULTS> -S <SYSLOG IP> -SP <SYSLOG PORT>"""
-    title = 'McAfee EDR Python API'
+    usage = """python mvision_edr_threats.py -R <REGION> -C <CLIENT_ID> -S <CLIENT_SECRET> -LL <LOG_LEVEL> """
+    title = 'MVISION EDR Python API'
     parser = ArgumentParser(description=title, usage=usage, formatter_class=RawTextHelpFormatter)
 
     parser.add_argument('--region', '-R',
                         required=True, type=str,
                         help='MVISION EDR Tenant Location', choices=['EU', 'US-W', 'US-E', 'SY', 'GOV'])
 
-    parser.add_argument('--user', '-U',
+    parser.add_argument('--client_id', '-C',
                         required=True, type=str,
-                        help='MVISION EDR Username')
+                        help='MVISION EDR Client ID')
 
-    parser.add_argument('--password', '-P',
+    parser.add_argument('--client_secret', '-S',
                         required=False, type=str,
-                        help='MVISION EDR Password')
+                        help='MVISION EDR Client Secret')
 
-    parser.add_argument('--limit', '-L',
-                        required=True, type=int,
-                        help='Maximum number of returned items')
+    parser.add_argument('--loglevel', '-LL',
+                        required=False, type=str, choices=['INFO', 'DEBUG'], default='INFO',
+                        help='Set Log Level')
 
     args = parser.parse_args()
-    if not args.password:
-        args.password = getpass.getpass(prompt='MVISION Password: ')
+    if not args.client_secret:
+        args.client_secret = getpass.getpass(prompt='MVISION EDR Client Secret: ')
 
     edr = EDR()
     edr.get_threats()
